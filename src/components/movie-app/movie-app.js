88ef1943b8050c8a332163abadaf6051a8195dc1
VorontsoV <voroncov12-93@mail.ru>
2022-11-07 00:11:07 +1000

    first commit

---
+++ src/components/movie-app/movie-app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class MovieApp extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return <h1>Movie App</h1>
+  }
+}
