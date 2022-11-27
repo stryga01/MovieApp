d58a0c9725a28aeeea8440b7ec1dcb65f77872e2
VorontsoV <voroncov12-93@mail.ru>
2022-11-27 22:22:28 +1000

    prod

--- src/components/Rated/Rated.js
+++ src/components/Rated/Rated.js
@@ -13,6 +13,7 @@ class Rated extends Component {
     this.state = {
       totalResults: null,
       currentPage: 1,
+      noContentAlert: false,
     }
   }
   componentDidMount() {
@@ -33,6 +34,11 @@ class Rated extends Component {
     setLoading(true)
     getRatedMovies(sessionId, currentPage)
       .then(({ results, total_results }) => {
+        if (!results.length) {
+          this.setState({
+            noContentAlert: true,
+          })
+        }
         this.setState({
           totalResults: total_results,
         })
@@ -51,12 +57,14 @@ class Rated extends Component {
 
   render() {
     const { dataRated, loading, sessionId, error } = this.props
-    const { totalResults, currentPage } = this.state
+    const { totalResults, currentPage, noContentAlert } = this.state
     const err = error ? (
       <Alert message="Что-то пошло не так, попробуйте повторить ваш запрос или включите VPN" type="error" />
     ) : null
+    const noContent = noContentAlert ? <Alert message="Вы не оценили ни одного фильма" type="info" /> : null
     return (
       <div className="ratedWrap">
+        {noContent}
         {err}
         {loading ? <Spinner /> : <MovieList movies={dataRated} sessionId={sessionId} />}
         <PaginationRenderer
